da6b2894974b3f5f40d35881e31ea86914f3d138
Some Code <somecodegithub@gmail.com>
2017-08-28 19:03:27 +0200

    rework cert generator (in progress)

--- cert/src/main/java/CertGeneratorKT.java
+++ cert/src/main/java/CertGeneratorKT.java
@@ -1,24 +1,36 @@
-import java.io.File;
+import setup.Constants;
+import setup.PathUtil;
+import sun.security.tools.keytool.CertAndKeyGen;
+import sun.security.x509.X500Name;
+
 import java.io.FileOutputStream;
+import java.lang.reflect.Method;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
-import setup.Constants;
-import setup.PathUtil;
-import sun.security.tools.keytool.CertAndKeyGen; // using keytool classes here...
-import sun.security.x509.X500Name;
 
 
-public class CertGenerator {
+/**
+ * this new variant of CertGenerator will invoke "keytool" instead of using internal classes.
+ * <p>
+ * invocation can happen in different ways, currently "main" is executed via reflection (assuming class is "just there" at runtime).
+ * Even more defensive would be to use ProcessBuilder to run keytool command in separate process.
+ * <p>
+ * this might also be an option https://github.com/mojohaus/keytool
+ * <p>
+ * Note that all this ugliness is caused by the fact that keytool does not offer a proper ("non command line") API
+ */
+public class CertGeneratorKT {
 
     public static void main(String[] args) throws Exception {
+
+
 
         // http://docs.oracle.com/javase/8/docs/technotes/tools/windows/keytool.html
         // http://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/JSSERefGuide.html
 
-        // TODO: maybe invoke Main.main("") or "Runtime.exec("keytool", "...")" -> see CertGeneratorKT
 
         // mimic some behavior of sun.security.tools.keytool.Main
         // A: Main.doGenKeyPair .. Generate self signed certificate
@@ -27,6 +39,38 @@ public class CertGenerator {
 
         // A: Generate self signed certificate
 
+//        invokeKeyTool("--help"); // works OK. help is written on "stderr"
+//        invokeKeyTool("-list"); // command errors are printed to "stdout", keytool does elegant System.exit(1)
+//        invokeKeyTool("-debug -list"); // this throws an exception
+
+
+        // args[0] in pom:  ${project.build.directory}
+        // path will be "../cert/target/classes"
+        String path = PathUtil.getTargetPath(CertGeneratorKT.class) + "classes/";
+
+        invokeKeyTool("-genkeypair" +
+                       " -keystore " + path + Constants.TLS_SERVER_KEY_STORE +
+                       " -storepass " + Constants.TLS_SERVER_KEY_PASS +
+                       " -validity " + 365 * Constants.TLS_CERT_VALIDITY_YEARS +
+                       " -alias " + Constants.TLS_SERVER_KEY_ALIAS +
+                       " -keyalg EC" +
+                       " -sigalg SHA256WithECDSA" +
+                       " -keysize 256" +
+                       " -keypass " + Constants.TLS_SERVER_KEY_PASS +
+                       " -dname CN=server01"
+
+        );
+
+        invokeKeyTool("-exportcert" +
+                       " -keystore " + path + Constants.TLS_SERVER_KEY_STORE +
+                       " -storepass " + Constants.TLS_SERVER_KEY_PASS +
+                       " -alias " + Constants.TLS_SERVER_KEY_ALIAS +
+                       " -file " + path + "server.pub"  // what format?
+        );
+
+        if (0==0) {
+            return;
+        }
 
         CertAndKeyGen keyGen = new CertAndKeyGen("EC", "SHA256WithECDSA");
         //   256 : secp256r1, 384: secp384r1, cannot change curve easily
@@ -54,9 +98,6 @@ public class CertGenerator {
 
         // todo: create CA for multiple clients
 
-        // args[0] in pom:  ${project.build.directory}
-        // path will be "../cert/target/classes"
-        String path = PathUtil.getTargetPath(CertGenerator.class) + File.separator + "classes" + File.separator;
 
         String serverAlias = Constants.TLS_SERVER_KEY_ALIAS;
         String clientAlias = "client";
@@ -94,5 +135,25 @@ public class CertGenerator {
             keyStore.store(new FileOutputStream(fileName4), password4);
         }
 
+    }
+
+
+    /**
+     * invoke keytool via reflection
+     * @param argLine blank separated list of parameters (like on command line)
+     */
+    private static void invokeKeyTool(String argLine) throws Exception {
+        System.out.println("using argLine: " + argLine);
+        invokeKeyTool(argLine.split(" "));
+    }
+
+    /**
+     * invoke keytool via reflection
+     * @param args args to hand over to keytool
+     */
+    private static void invokeKeyTool(String[] args) throws Exception {
+        Class clazz = Class.forName("sun.security.tools.keytool.Main");
+        Method meth = clazz.getMethod("main", String[].class);
+        meth.invoke(null, (Object) args); // static method doesn't have an instance
     }
 }
